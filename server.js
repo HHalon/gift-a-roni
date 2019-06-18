7eca9f35c6cc605c727902f926540f329d9bfd4d
Jake Smith <jacobwsmith19@gmail.com>
2019-06-18 14:16:39 -0500

    Added server.js, set up folders and routes files

---
+++ server.js
@@ -0,0 +1,34 @@
+// =============================================================
+// Dependencies
+// =============================================================
+var express = require("express");
+
+// =============================================================
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requires the models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/html-routes.js")(app);
+
+// *** need to add API routes
+
+// Syncs the sequelize models and starts the Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
